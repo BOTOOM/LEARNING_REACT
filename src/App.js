d86d6788c85bb0d33268b1767b582f21cca77b62
BOTOOM <eddiazr@correo.udistrital.edu.co>
2018-10-02 14:10:49 -0500

    se agrega appBar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import {Grid, Row, Col} from 'react-flexbox-grid';
 import LocationList from './Componentes/LocationList'
 import './App.css';
@@ -18,7 +21,15 @@ class App extends Component {
   render() {
     return (
         <Grid fluid>
-
+          <Row>
+            <AppBar position="sticky" >
+              <Toolbar>
+                <Typography variant="title" color="inherit">
+                  Weather App
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </Row>
           <Row>
             <Col xs={12} md={6}>
             <LocationList
